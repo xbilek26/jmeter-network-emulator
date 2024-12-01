31b28eaac733125ec8fd1e71e63c05cf98fa1c8d
xbilek26 <bilekf@email.cz>
2024-12-01 23:04:10 +0100

    update NetworkEmulatorController.java

--- src/main/java/cz/vutbr/networkemulator/controller/NetworkEmulatorController.java
+++ src/main/java/cz/vutbr/networkemulator/controller/NetworkEmulatorController.java
@@ -2,38 +2,31 @@ package cz.vutbr.networkemulator.controller;
 
 import java.util.ArrayList;
 import java.util.Enumeration;
+import java.io.Serializable;
 import java.net.NetworkInterface;
 import java.net.SocketException;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class NetworkEmulatorController {
+public class NetworkEmulatorController implements Serializable {
 
+    private static final long serialVersionUID = 1L;
+
+    @SuppressWarnings("unused")
     private static final Logger log = LoggerFactory.getLogger(NetworkEmulatorController.class);
 
-    public ArrayList<String> networkInterfaces = new ArrayList<String>();
-
-    public NetworkEmulatorController() {
-        try {
-            loadNetworkInterfaces();
-            log.info("Network interfaces succesfully loaded." + networkInterfaces.toString());
-        } catch (SocketException e) {
-            log.error("Error loading network interfaces: " + e.getMessage());
-        }
-    }
-
-    private void loadNetworkInterfaces() throws SocketException {
+    public ArrayList<String> getNetworkInterfacesNames() throws SocketException {
         Enumeration<NetworkInterface> networkInterfaces = NetworkInterface.getNetworkInterfaces();
+        ArrayList<String> networkInterfacesNames = new ArrayList<>();
         while (networkInterfaces.hasMoreElements()) {
             NetworkInterface networkInterface = networkInterfaces.nextElement();
-            if (networkInterface.isUp() && !networkInterface.isLoopback()) {
-                this.networkInterfaces.add(networkInterface.getName());
-            }
+            // uncomment in future
+            //if (networkInterface.isUp() && !networkInterface.isLoopback()) {
+                networkInterfacesNames.add(networkInterface.getName());
+            //}
         }
-    }
 
-    public ArrayList<String> getNetworkInterfaces() {
-        return networkInterfaces;
+        return networkInterfacesNames;
     }
 }
