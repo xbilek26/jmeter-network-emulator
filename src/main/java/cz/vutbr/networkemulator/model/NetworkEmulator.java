ab401c5afd0cb8d5b2d5451f60068c18a2252030
xbilek26 <bilekf@email.cz>
2024-12-01 23:04:51 +0100

    update NetworkEmulator.java

--- src/main/java/cz/vutbr/networkemulator/model/NetworkEmulator.java
+++ src/main/java/cz/vutbr/networkemulator/model/NetworkEmulator.java
@@ -1,35 +1,39 @@
 package cz.vutbr.networkemulator.model;
 
+import cz.vutbr.networkemulator.controller.NetworkEmulatorController;
+
 import org.apache.jmeter.testelement.AbstractTestElement;
 
+import java.net.SocketException;
 import java.util.ArrayList;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class NetworkEmulator extends AbstractTestElement {
 
-    private ArrayList<String> availableNetworkInterfaces;
-    private ArrayList<String> addedInterfaces;
-    public void setAvaiableNetworkInterfaces(ArrayList<String> availableNetworkInterfaces) {
-        this.availableNetworkInterfaces = availableNetworkInterfaces;
-    }
+    private static final Logger log = LoggerFactory.getLogger(NetworkEmulator.class);
+
+    private NetworkEmulatorController networkEmulatorController;
+    private ArrayList<String> availableNetworkInterfaces = new ArrayList<>();
 
     public NetworkEmulator() {
-        availableNetworkInterfaces = new ArrayList<>();
-        addedInterfaces = new ArrayList<>();
+        networkEmulatorController = new NetworkEmulatorController();
+        try {
+            setAvaiableNetworkInterfaces(networkEmulatorController.getNetworkInterfacesNames());
+        } catch (SocketException e) {
+            log.error("Error loading network interfaces: " + e.getMessage());
+        }
+    }
+
+    public void setAvaiableNetworkInterfaces(ArrayList<String> interfaces) {
+        if (interfaces != null) {
+            availableNetworkInterfaces.clear();
+            availableNetworkInterfaces.addAll(interfaces);
+        }
     }
 
     public ArrayList<String> getAvailableNetworkInterfaces() {
-        return availableNetworkInterfaces;
-    }
-
-    public ArrayList<String> getAddedInterfaces() {
-        return addedInterfaces;
-    }
-
-    public boolean isInterfaceAdded(String networkInterface) {
-        return addedInterfaces.contains(networkInterface);
-    }
-
-    public ArrayList<String> getAddedNetworkInterfaces() {
-        return addedInterfaces;
+        return new ArrayList<>(availableNetworkInterfaces);
     }
 }
