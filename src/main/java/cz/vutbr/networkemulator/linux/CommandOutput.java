de04bec8465e9d0dab9d75bdffbfbff9023880d1
xbilek26 <bilekf@email.cz>
2025-02-24 02:09:43 +0100

    feat: create CommandOutput.java

---
+++ src/main/java/cz/vutbr/networkemulator/linux/CommandOutput.java
@@ -0,0 +1,11 @@
+package cz.vutbr.networkemulator.linux;
+
+public class CommandOutput {
+
+    String outputString;
+
+    public CommandOutput(String outputString) {
+        this.outputString = outputString;
+    }
+
+}
