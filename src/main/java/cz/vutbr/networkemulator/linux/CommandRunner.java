820c712e1498287f1d3f99036057da2715810981
xbilek26 <bilekf@email.cz>
2025-02-25 01:59:49 +0100

    update CommandRunner.java

--- src/main/java/cz/vutbr/networkemulator/linux/CommandRunner.java
+++ src/main/java/cz/vutbr/networkemulator/linux/CommandRunner.java
@@ -2,32 +2,36 @@ package cz.vutbr.networkemulator.linux;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.List;
 
 public class CommandRunner {
 
     public CommandOutput runCommand(String command) {
 
-        String output = "";
+        List<String> commandList = Arrays.asList(command.split("\\s+"));
+
+        StringBuilder output = new StringBuilder();
 
         try {
-            ProcessBuilder processBuilder = new ProcessBuilder(List.of("tc", "qdisc"));
+            ProcessBuilder processBuilder = new ProcessBuilder(commandList);
             Process process = processBuilder.start();
             BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
-            String line;
+            String line = "start";
+            
             while ((line = reader.readLine()) != null) {
-                output.concat(line);
-                output.concat("\n");
+                output.append(line).append("\n");
             }
-            process.waitFor();
 
-            return new CommandOutput(output);
+            process.waitFor();
+            return new CommandOutput(output.toString().trim());
 
         } catch (Exception e) {
             e.printStackTrace();
+            System.out.println("error");
         }
 
-        return null;
+        return new CommandOutput("null");
     }
 
 
