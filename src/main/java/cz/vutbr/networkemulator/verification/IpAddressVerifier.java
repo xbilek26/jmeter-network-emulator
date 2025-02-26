fabca43ddd61eeeae2e6509b64301c6b1e336be6
xbilek26 <bilekf@email.cz>
2025-02-26 03:06:32 +0100

    feat: create IpAddressVerifier.java

---
+++ src/main/java/cz/vutbr/networkemulator/verification/IpAddressVerifier.java
@@ -0,0 +1,14 @@
+package cz.vutbr.networkemulator.verification;
+
+import javax.swing.InputVerifier;
+import javax.swing.JComponent;
+
+public class IpAddressVerifier extends InputVerifier {
+
+    @Override
+    public boolean verify(JComponent input) {
+        // TODO
+        return true;
+    }
+
+}
