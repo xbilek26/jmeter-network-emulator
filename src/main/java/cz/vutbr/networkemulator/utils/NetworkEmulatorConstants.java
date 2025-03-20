60f40c071f61fc3d479511d37f50698543482b77
xbilek26 <bilekf@email.cz>
2025-03-20 06:39:16 +0100

    feat: create NetworkEmulatorConstants.java

---
+++ src/main/java/cz/vutbr/networkemulator/utils/NetworkEmulatorConstants.java
@@ -0,0 +1,23 @@
+package cz.vutbr.networkemulator.utils;
+
+public final class NetworkEmulatorConstants {
+    public static final String BTN_ADD_TRAFFIC_CLASS = "Add Traffic Class";
+    public static final String BTN_REMOVE_TRAFFIC_CLASS = "Remove Traffic Class";
+    public static final String BTN_REFRESH_INTERFACES = "Refresh Interfaces";
+    public static final String EMULATOR_NAME = "Network Emulator";
+    public static final String MSG_ERROR = "Error";
+    public static final String MSG_NO_INTERFACE_SELECTED = "No Interface Selected!";
+    public static final String MSG_EMULATION_RUNNING = "Emulation is Running.";
+    public static final String MSG_EMULATION_STOPPED = "Emulation is stopped.";
+    public static final String TITLE_CONFIG = "Configuration Panel";
+    public static final String TITLE_MAIN_PANEL = "Main Panel";
+    public static final String MSG_ENTER_TRAFFIC_CLASS_NAME = "Enter traffic class name:";
+    public static final String MSG_ENTER_VALID_IP_ADDRESS = "Enter a valid IPv4 or IPv6 Address.";
+    public static final String BAD_ADDRESS = "Bad Address";
+    public static final String ROOT_NODE_NAME = "Network Interfaces";
+    public static final String BTN_START_EMULATION = "Start";
+    public static final String BTN_STOP_EMULATION = "Stop";
+
+    private NetworkEmulatorConstants() {
+    }
+}
