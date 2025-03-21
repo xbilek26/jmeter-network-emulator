121eb5d115d77a06e36c54561ba973c3cf7e9dc7
xbilek26 <bilekf@email.cz>
2025-03-21 03:39:34 +0100

    update NetworkEmulatorConstants.java

--- src/main/java/cz/vutbr/networkemulator/utils/NetworkEmulatorConstants.java
+++ src/main/java/cz/vutbr/networkemulator/utils/NetworkEmulatorConstants.java
@@ -4,19 +4,48 @@ public final class NetworkEmulatorConstants {
     public static final String BTN_ADD_TRAFFIC_CLASS = "Add Traffic Class";
     public static final String BTN_REMOVE_TRAFFIC_CLASS = "Remove Traffic Class";
     public static final String BTN_REFRESH_INTERFACES = "Refresh Interfaces";
-    public static final String EMULATOR_NAME = "Network Emulator";
+    public static final String NETWORK_EMULATOR_STATIC_LABEL = "Network Emulator";
+    public static final String NETWORK_EMULATOR_LABEL_RESOURCE = "network_emulator";
+
     public static final String MSG_ERROR = "Error";
     public static final String MSG_NO_INTERFACE_SELECTED = "No Interface Selected!";
     public static final String MSG_EMULATION_RUNNING = "Emulation is Running.";
     public static final String MSG_EMULATION_STOPPED = "Emulation is stopped.";
-    public static final String TITLE_CONFIG = "Configuration Panel";
-    public static final String TITLE_MAIN_PANEL = "Main Panel";
     public static final String MSG_ENTER_TRAFFIC_CLASS_NAME = "Enter traffic class name:";
     public static final String MSG_ENTER_VALID_IP_ADDRESS = "Enter a valid IPv4 or IPv6 Address.";
     public static final String BAD_ADDRESS = "Bad Address";
     public static final String ROOT_NODE_NAME = "Network Interfaces";
     public static final String BTN_START_EMULATION = "Start";
     public static final String BTN_STOP_EMULATION = "Stop";
+
+    public static final String TITLE_MAIN_PANEL = "Main Panel";
+    public static final String TITLE_CONFIG_PANEL = "Configuration Panel";
+    public static final String TITLE_FILTER_PANEL = "Filter";
+    public static final String TITLE_DELAY_PANEL = "Delay";
+    public static final String TITLE_DROP_PANEL = "Packet Drop";
+    public static final String TITLE_RATE_PANEL = "Rate";
+    public static final String TITLE_LOSS_PANEL = "Loss";
+    public static final String TITLE_REORDERING_PANEL = "Reordering";
+    public static final String TITLE_DUPLICATION_PANEL = "Duplication";
+    public static final String TITLE_CORRUPTION_PANEL = "Corruption";
+
+    public static final String LABEL_SRC_ADDRESS = "Src Address:";
+    public static final String LABEL_SRC_PORT = "Src Port:";
+    public static final String LABEL_DST_ADDRESS = "Dst Address:";
+    public static final String LABEL_DST_PORT = "Dst Port:";
+    public static final String LABEL_DELAY_VALUE = "Startup Delay (ms):";
+    public static final String LABEL_JITTER = "Jitter (ms):";
+    public static final String LABEL_DELAY_CORRELATION = "Correlation (%):";
+    public static final String LABEL_DISTRIBUTION = "Distribution:";
+    public static final String LABEL_DROP_VALUE = "Packet Drop (%):";
+    public static final String LABEL_DROP_CORRELATION = "Correlation (%):";
+    public static final String LABEL_RATE = "Rate (bps):";
+    public static final String LABEL_LOSS = "Loss (%):";
+    public static final String LABEL_REORDERING_VALUE = "Reordering (%):";
+    public static final String LABEL_REORDERING_CORRELATION = "Correlation (%):";
+    public static final String LABEL_DUPLICATION_VALUE = "Duplication (%):";
+    public static final String LABEL_DUPLICATION_CORRELATION = "Correlation (%):";
+    public static final String LABEL_CORRUPTION = "Corruption (%):";
 
     private NetworkEmulatorConstants() {
     }
