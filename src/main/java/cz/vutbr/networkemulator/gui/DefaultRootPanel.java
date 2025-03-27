1d5408225e2a1829c21deeb4d5711c65c40cf069
xbilek26 <bilekf@email.cz>
2025-03-27 06:38:51 +0100

    feat: create DefaultRootPanel.java

---
+++ src/main/java/cz/vutbr/networkemulator/gui/DefaultRootPanel.java
@@ -0,0 +1,10 @@
+package cz.vutbr.networkemulator.gui;
+
+import javax.swing.JPanel;
+
+public class DefaultRootPanel extends JPanel {
+
+    void update() {
+        // throw new UnsupportedOperationException("Not supported yet.");
+    }
+}
