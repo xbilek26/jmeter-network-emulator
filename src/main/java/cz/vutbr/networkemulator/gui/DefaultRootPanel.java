662ad6b32fb98e4f0cdf748d355e0df6f338b02f
xbilek26 <bilekf@email.cz>
2025-03-29 20:24:31 +0100

    Add constructor to DefaultRootPanel to set panel name

--- src/main/java/cz/vutbr/networkemulator/gui/DefaultRootPanel.java
+++ src/main/java/cz/vutbr/networkemulator/gui/DefaultRootPanel.java
@@ -4,6 +4,10 @@ import javax.swing.JPanel;
 
 public class DefaultRootPanel extends JPanel {
 
+    public DefaultRootPanel(String name) {
+        setName(name);
+    }
+
     void update() {
         // throw new UnsupportedOperationException("Not supported yet.");
     }
