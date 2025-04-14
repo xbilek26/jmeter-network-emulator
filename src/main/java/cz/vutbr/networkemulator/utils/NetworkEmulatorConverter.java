b8b2e6af01810c946f3fef319e5614539dd812bc
xbilek26 <bilekf@email.cz>
2025-04-14 02:46:01 +0200

    Refactor NetworkEmulatorConverter to replace drop terminology with loss for consistency

--- src/main/java/cz/vutbr/networkemulator/utils/NetworkEmulatorConverter.java
+++ src/main/java/cz/vutbr/networkemulator/utils/NetworkEmulatorConverter.java
@@ -21,15 +21,14 @@ public class NetworkEmulatorConverter {
         networkParameters.setDelayValue(NumberUtils.toInt(propertyParameters.get(4).getStringValue(), -1));
         networkParameters.setJitter(NumberUtils.toInt(propertyParameters.get(5).getStringValue(), -1));
         networkParameters.setDelayCorrelation(NumberUtils.toInt(propertyParameters.get(6).getStringValue(), -1));
-        networkParameters.setDropValue(NumberUtils.toInt(propertyParameters.get(7).getStringValue(), -1));
-        networkParameters.setDropCorrelation(NumberUtils.toInt(propertyParameters.get(8).getStringValue(), -1));
+        networkParameters.setLossValue(NumberUtils.toInt(propertyParameters.get(7).getStringValue(), -1));
+        networkParameters.setLossCorrelation(NumberUtils.toInt(propertyParameters.get(8).getStringValue(), -1));
         networkParameters.setRate(NumberUtils.toInt(propertyParameters.get(9).getStringValue(), -1));
-        networkParameters.setLoss(NumberUtils.toInt(propertyParameters.get(10).getStringValue(), -1));
-        networkParameters.setReorderingValue(NumberUtils.toInt(propertyParameters.get(11).getStringValue(), -1));
-        networkParameters.setReorderingCorrelation(NumberUtils.toInt(propertyParameters.get(12).getStringValue(), -1));
-        networkParameters.setDuplicationValue(NumberUtils.toInt(propertyParameters.get(13).getStringValue(), -1));
-        networkParameters.setDuplicationCorrelation(NumberUtils.toInt(propertyParameters.get(14).getStringValue(), -1));
-        networkParameters.setCorruption(NumberUtils.toInt(propertyParameters.get(15).getStringValue(), -1));
+        networkParameters.setReorderingValue(NumberUtils.toInt(propertyParameters.get(10).getStringValue(), -1));
+        networkParameters.setReorderingCorrelation(NumberUtils.toInt(propertyParameters.get(11).getStringValue(), -1));
+        networkParameters.setDuplicationValue(NumberUtils.toInt(propertyParameters.get(12).getStringValue(), -1));
+        networkParameters.setDuplicationCorrelation(NumberUtils.toInt(propertyParameters.get(13).getStringValue(), -1));
+        networkParameters.setCorruption(NumberUtils.toInt(propertyParameters.get(14).getStringValue(), -1));
     
         return networkParameters;
     }
