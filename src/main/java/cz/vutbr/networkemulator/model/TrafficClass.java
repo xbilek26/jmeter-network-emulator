f1b8e5e3869566d400e380b453d91b010bab478a
xbilek26 <bilekf@email.cz>
2025-04-16 05:17:05 +0200

    Add TrafficClass model to represent network parameters

---
+++ src/main/java/cz/vutbr/networkemulator/model/TrafficClass.java
@@ -0,0 +1,38 @@
+package cz.vutbr.networkemulator.model;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Represents a traffic class that holds network parameters.
+ *
+ * @author Frantisek Bilek (xbilek26)
+ */
+public class TrafficClass {
+
+    @SuppressWarnings("unused")
+    private static final Logger log = LoggerFactory.getLogger(TrafficClass.class);
+
+    String name;
+    NetworkParameters networkParameters;
+
+    public TrafficClass(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public NetworkParameters getNetworkParameters() {
+        return networkParameters;
+    }
+
+    public void setNetworkParameters(NetworkParameters networkParameters) {
+        this.networkParameters = networkParameters;
+    }
+}
