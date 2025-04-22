7f5d1f6be0163531a49c54966e612f36db7af9c2
xbilek26 <bilekf@email.cz>
2025-04-22 08:57:06 +0200

    Refactor convertToNetworkParameters method to include ipProtocol and dstSubnetMask fields

--- src/main/java/cz/vutbr/networkemulator/utils/NetworkEmulatorConverter.java
+++ src/main/java/cz/vutbr/networkemulator/utils/NetworkEmulatorConverter.java
@@ -12,24 +12,28 @@ public class NetworkEmulatorConverter {
     @SuppressWarnings("unused")
     private static final Logger log = LoggerFactory.getLogger(NetworkEmulatorConverter.class);
 
-    public static NetworkParameters convertToNetworkParameters(CollectionProperty propertyParameters) {
-        NetworkParameters networkParameters = new NetworkParameters();
-        networkParameters.setSrcAddress(propertyParameters.get(0).getStringValue());
-        networkParameters.setSrcPort(NumberUtils.toInt(propertyParameters.get(1).getStringValue(), -1));
-        networkParameters.setDstAddress(propertyParameters.get(2).getStringValue());
-        networkParameters.setDstPort(NumberUtils.toInt(propertyParameters.get(3).getStringValue(), -1));
-        networkParameters.setDelayValue(NumberUtils.toInt(propertyParameters.get(4).getStringValue(), -1));
-        networkParameters.setJitter(NumberUtils.toInt(propertyParameters.get(5).getStringValue(), -1));
-        networkParameters.setDelayCorrelation(NumberUtils.toInt(propertyParameters.get(6).getStringValue(), -1));
-        networkParameters.setLossValue(NumberUtils.toInt(propertyParameters.get(7).getStringValue(), -1));
-        networkParameters.setLossCorrelation(NumberUtils.toInt(propertyParameters.get(8).getStringValue(), -1));
-        networkParameters.setRate(NumberUtils.toInt(propertyParameters.get(9).getStringValue(), -1));
-        networkParameters.setReorderingValue(NumberUtils.toInt(propertyParameters.get(10).getStringValue(), -1));
-        networkParameters.setReorderingCorrelation(NumberUtils.toInt(propertyParameters.get(11).getStringValue(), -1));
-        networkParameters.setDuplicationValue(NumberUtils.toInt(propertyParameters.get(12).getStringValue(), -1));
-        networkParameters.setDuplicationCorrelation(NumberUtils.toInt(propertyParameters.get(13).getStringValue(), -1));
-        networkParameters.setCorruption(NumberUtils.toInt(propertyParameters.get(14).getStringValue(), -1));
+    public static NetworkParameters convertToNetworkParameters(CollectionProperty propertyParams) {
+        NetworkParameters params = new NetworkParameters();
+        params.setIpProtocol(propertyParams.get(0).getStringValue());
+        params.setSrcAddress(propertyParams.get(1).getStringValue());
+        params.setSrcSubnetMask(propertyParams.get(2).getStringValue());
+        params.setSrcPort(NumberUtils.toInt(propertyParams.get(3).getStringValue(), -1));
+        params.setDstAddress(propertyParams.get(4).getStringValue());
+        params.setDstSubnetMask(propertyParams.get(5).getStringValue());
+        params.setDstPort(NumberUtils.toInt(propertyParams.get(6).getStringValue(), -1));
+        params.setDelayValue(NumberUtils.toInt(propertyParams.get(7).getStringValue(), -1));
+        params.setJitter(NumberUtils.toInt(propertyParams.get(8).getStringValue(), -1));
+        params.setDelayCorrelation(NumberUtils.toInt(propertyParams.get(9).getStringValue(), -1));
+        params.setDistribution(propertyParams.get(10).getStringValue());
+        params.setLossValue(NumberUtils.toInt(propertyParams.get(11).getStringValue(), -1));
+        params.setLossCorrelation(NumberUtils.toInt(propertyParams.get(12).getStringValue(), -1));
+        params.setRate(NumberUtils.toInt(propertyParams.get(13).getStringValue(), -1));
+        params.setReorderingValue(NumberUtils.toInt(propertyParams.get(14).getStringValue(), -1));
+        params.setReorderingCorrelation(NumberUtils.toInt(propertyParams.get(15).getStringValue(), -1));
+        params.setDuplicationValue(NumberUtils.toInt(propertyParams.get(16).getStringValue(), -1));
+        params.setDuplicationCorrelation(NumberUtils.toInt(propertyParams.get(17).getStringValue(), -1));
+        params.setCorruption(NumberUtils.toInt(propertyParams.get(18).getStringValue(), -1));
     
-        return networkParameters;
+        return params;
     }
 }
