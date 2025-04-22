2c6db9d78b8fd302178fc7364c87edc93219094c
xbilek26 <bilekf@email.cz>
2025-04-22 08:55:43 +0200

    Add ConfigTree class: implement expanded path management and selection functionality

---
+++ src/main/java/cz/vutbr/networkemulator/gui/tree/ConfigTree.java
@@ -0,0 +1,113 @@
+package cz.vutbr.networkemulator.gui.tree;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.JTree;
+import javax.swing.tree.TreeModel;
+import javax.swing.tree.TreePath;
+
+public class ConfigTree extends JTree {
+
+    public ConfigTree(TreeModel model) {
+        super(model);
+    }
+
+    public List<String> getExpandedPaths() {
+        List<String> expanded = new ArrayList<>();
+        ConfigTreeNode rootNode = (ConfigTreeNode) getModel().getRoot();
+        TreePath rootPath = new TreePath(rootNode);
+
+        if (isExpanded(rootPath)) {
+            expanded.add(rootNode.getName());
+        }
+
+        int niCount = treeModel.getChildCount(rootNode);
+        for (int i = 0; i < niCount; i++) {
+            Object networkInterface = treeModel.getChild(rootNode, i);
+            TreePath niPath = rootPath.pathByAddingChild(networkInterface);
+
+            if (isExpanded(niPath)) {
+                expanded.add(networkInterface.toString());
+            }
+
+            int classCount = treeModel.getChildCount(networkInterface);
+            for (int j = 0; j < classCount; j++) {
+                Object trafficClass = treeModel.getChild(networkInterface, j);
+                TreePath classPath = niPath.pathByAddingChild(trafficClass);
+
+                if (isExpanded(classPath)) {
+                    expanded.add(networkInterface.toString() + "/" + trafficClass.toString());
+                }
+            }
+        }
+
+        return expanded;
+    }
+
+    public void restoreExpandedPaths(List<String> pathsToExpand) {
+        ConfigTreeNode rootNode = (ConfigTreeNode) getModel().getRoot();
+        TreePath rootPath = new TreePath(rootNode);
+
+        if (pathsToExpand.contains(rootNode.getName())) {
+            expandPath(rootPath);
+        }
+
+        int niCount = treeModel.getChildCount(rootNode);
+        for (int i = 0; i < niCount; i++) {
+            Object ni = treeModel.getChild(rootNode, i);
+            TreePath niPath = rootPath.pathByAddingChild(ni);
+
+            if (pathsToExpand.contains(ni.toString())) {
+                expandPath(niPath);
+            }
+
+            int tcCount = treeModel.getChildCount(ni);
+            for (int j = 0; j < tcCount; j++) {
+                Object tc = treeModel.getChild(ni, j);
+                String pathStr = ni.toString() + "/" + tc.toString();
+
+                if (pathsToExpand.contains(pathStr)) {
+                    TreePath tcPath = niPath.pathByAddingChild(tc);
+                    expandPath(tcPath);
+                }
+            }
+        }
+    }
+
+    public String getSelectedPath() {
+        TreePath selectedPath = getSelectionPath();
+        Object[] pathComponents = selectedPath.getPath();
+        StringBuilder pathBuilder = new StringBuilder();
+        for (Object pathComponent : pathComponents) {
+            ConfigTreeNode node = (ConfigTreeNode) pathComponent;
+            pathBuilder.append(node.getName());
+            pathBuilder.append("/");
+        }
+
+        return pathBuilder.toString();
+    }
+
+    public void selectPath(String pathToSelect) {
+        ConfigTreeNode rootNode = (ConfigTreeNode) getModel().getRoot();
+        String[] pathComponents = pathToSelect.split("/");
+
+        TreePath path = new TreePath(rootNode);
+        ConfigTreeNode currentNode = rootNode;
+
+        for (String pathComponent : pathComponents) {
+            for (int i = 0; i < currentNode.getChildCount(); i++) {
+                ConfigTreeNode child = (ConfigTreeNode) currentNode.getChildAt(i);
+                if (pathComponent.equals(child.getName())) {
+                    path = path.pathByAddingChild(child);
+                    currentNode = child;
+                    break;
+                }
+            }
+        }
+
+        setSelectionPath(path);
+        scrollPathToVisible(path);
+    }
+
+}
