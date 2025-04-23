e3b7ec2a23967b660871509fd817b8141f775488
xbilek26 <bilekf@email.cz>
2025-04-23 08:04:46 +0200

    Refactor NetworkEmulatorConverter: change parameter type from CollectionProperty to List for improved clarity and simplify conversion methods

--- src/main/java/cz/vutbr/networkemulator/utils/NetworkEmulatorConverter.java
+++ src/main/java/cz/vutbr/networkemulator/utils/NetworkEmulatorConverter.java
@@ -1,7 +1,11 @@
 package cz.vutbr.networkemulator.utils;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.apache.commons.lang3.math.NumberUtils;
 import org.apache.jmeter.testelement.property.CollectionProperty;
+import org.apache.jmeter.testelement.property.StringProperty;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -12,28 +16,47 @@ public class NetworkEmulatorConverter {
     @SuppressWarnings("unused")
     private static final Logger log = LoggerFactory.getLogger(NetworkEmulatorConverter.class);
 
-    public static NetworkParameters convertToNetworkParameters(CollectionProperty propertyParams) {
+    public static NetworkParameters convertToNetworkParameters(List<String> listParams) {
         NetworkParameters params = new NetworkParameters();
-        params.setIpProtocol(propertyParams.get(0).getStringValue());
-        params.setSrcAddress(propertyParams.get(1).getStringValue());
-        params.setSrcSubnetMask(propertyParams.get(2).getStringValue());
-        params.setSrcPort(NumberUtils.toInt(propertyParams.get(3).getStringValue(), -1));
-        params.setDstAddress(propertyParams.get(4).getStringValue());
-        params.setDstSubnetMask(propertyParams.get(5).getStringValue());
-        params.setDstPort(NumberUtils.toInt(propertyParams.get(6).getStringValue(), -1));
-        params.setDelayValue(NumberUtils.toInt(propertyParams.get(7).getStringValue(), -1));
-        params.setJitter(NumberUtils.toInt(propertyParams.get(8).getStringValue(), -1));
-        params.setDelayCorrelation(NumberUtils.toInt(propertyParams.get(9).getStringValue(), -1));
-        params.setDistribution(propertyParams.get(10).getStringValue());
-        params.setLossValue(NumberUtils.toInt(propertyParams.get(11).getStringValue(), -1));
-        params.setLossCorrelation(NumberUtils.toInt(propertyParams.get(12).getStringValue(), -1));
-        params.setRate(NumberUtils.toInt(propertyParams.get(13).getStringValue(), -1));
-        params.setReorderingValue(NumberUtils.toInt(propertyParams.get(14).getStringValue(), -1));
-        params.setReorderingCorrelation(NumberUtils.toInt(propertyParams.get(15).getStringValue(), -1));
-        params.setDuplicationValue(NumberUtils.toInt(propertyParams.get(16).getStringValue(), -1));
-        params.setDuplicationCorrelation(NumberUtils.toInt(propertyParams.get(17).getStringValue(), -1));
-        params.setCorruption(NumberUtils.toInt(propertyParams.get(18).getStringValue(), -1));
-    
+        params.setIpProtocol(listParams.get(0));
+        params.setSrcAddress(listParams.get(1));
+        params.setSrcSubnetMask(listParams.get(2));
+        params.setSrcPort(NumberUtils.toInt(listParams.get(3), -1));
+        params.setDstAddress(listParams.get(4));
+        params.setDstSubnetMask(listParams.get(5));
+        params.setDstPort(NumberUtils.toInt(listParams.get(6), -1));
+        params.setDelayValue(NumberUtils.toInt(listParams.get(7), -1));
+        params.setJitter(NumberUtils.toInt(listParams.get(8), -1));
+        params.setDelayCorrelation(NumberUtils.toInt(listParams.get(9), -1));
+        params.setDistribution(listParams.get(10));
+        params.setLossValue(NumberUtils.toInt(listParams.get(11), -1));
+        params.setLossCorrelation(NumberUtils.toInt(listParams.get(12), -1));
+        params.setRate(NumberUtils.toInt(listParams.get(13), -1));
+        params.setReorderingValue(NumberUtils.toInt(listParams.get(14), -1));
+        params.setReorderingCorrelation(NumberUtils.toInt(listParams.get(15), -1));
+        params.setDuplicationValue(NumberUtils.toInt(listParams.get(16), -1));
+        params.setDuplicationCorrelation(NumberUtils.toInt(listParams.get(17), -1));
+        params.setCorruption(NumberUtils.toInt(listParams.get(18), -1));
+
         return params;
     }
+
+    public static List<String> convertToList(CollectionProperty property) {
+        List<String> list = new ArrayList<>();
+        if (property == null) {
+            return list;
+        }
+        for (int i = 0; i < property.size(); i++) {
+            list.add(property.get(i).getStringValue());
+        }
+        return list;
+    }
+
+    public static String convertToString(StringProperty property) {
+        if (property == null) {
+            return "";
+        }
+        return property.getStringValue();
+    }
+
 }
