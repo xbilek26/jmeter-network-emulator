fe09c834baff52d42de71c541c6666dd3ff3282b
xbilek26 <bilekf@email.cz>
2025-04-23 08:04:36 +0200

    Refactor TrafficControl: simplify parameter handling in traffic setup methods and improve readability of command construction

--- src/main/java/cz/vutbr/networkemulator/linux/TrafficControl.java
+++ src/main/java/cz/vutbr/networkemulator/linux/TrafficControl.java
@@ -17,65 +17,67 @@ public class TrafficControl {
         StringBuilder cmd = new StringBuilder();
         cmd.append(String.format("tc qdisc add dev %s parent %s handle %s netem", dev, classId, handleId));
 
-        appendDelay(cmd, params.getDelayValue(), params.getJitter(), params.getDelayCorrelation(), params.getDistribution());
-        appendLoss(cmd, params.getLossValue(), params.getLossCorrelation());
-        appendRate(cmd, params.getRate());
-        appendReordering(cmd, params.getReorderingValue(), params.getReorderingCorrelation());
-        appendDuplication(cmd, params.getDuplicationValue(), params.getDuplicationCorrelation());
-        appendCorruption(cmd, params.getCorruption());
+        appendDelay(cmd, params);
+        appendLoss(cmd, params);
+        appendRate(cmd, params);
+        appendReordering(cmd, params);
+        appendDuplication(cmd, params);
+        appendCorruption(cmd, params);
 
         CommandRunner.runCommand(cmd.toString());
     }
 
-    private static void appendDelay(StringBuilder cmd, int delay, int jitter, int correlation, String distribution) {
-        if (delay != -1) {
-            cmd.append(String.format(" delay %sms", delay));
-            if (jitter != -1) {
-                cmd.append(String.format(" %sms", jitter));
-                if (correlation != -1) {
-                    cmd.append(String.format(" %s%%", correlation));
+    private static void appendDelay(StringBuilder cmd, NetworkParameters params) {
+        if (params.isDelayValueSet()) {
+            cmd.append(String.format(" delay %sms", params.getDelayValue()));
+            if (params.isJitterSet()) {
+                cmd.append(String.format(" %sms", params.getJitter()));
+                if (params.isDelayCorrelationSet()) {
+                    cmd.append(String.format(" %s%%", params.getDelayCorrelation()));
                 }
-                cmd.append(String.format(" distribution %s", distribution));
+                if (params.isDistributionSet()) {
+                    cmd.append(String.format(" distribution %s", params.getDistribution()));
+                }
             }
         }
     }
 
-    private static void appendLoss(StringBuilder cmd, int loss, int correlation) {
-        if (loss != -1) {
-            cmd.append(String.format(" loss %s%%", loss));
-            if (correlation != -1) {
-                cmd.append(String.format(" %s%%", correlation));
+    private static void appendLoss(StringBuilder cmd, NetworkParameters params) {
+        if (params.isLossValueSet()) {
+            cmd.append(String.format(" loss %s%%", params.getLossValue()));
+            if (params.isLossCorrelationSet()) {
+                cmd.append(String.format(" %s%%", params.getLossCorrelation()));
             }
         }
     }
 
-    private static void appendRate(StringBuilder cmd, int rate) {
-        if (rate != -1) {
-            cmd.append(String.format(" rate %skbit", rate));
+    private static void appendRate(StringBuilder cmd, NetworkParameters params) {
+        if (params.isRateSet()) {
+            cmd.append(String.format(" rate %skbit", params.getRate()));
         }
     }
 
-    private static void appendReordering(StringBuilder cmd, int reordering, int correlation) {
-        if (reordering != -1) {
-            cmd.append(String.format(" reorder %s%%", reordering));
-            if (correlation != -1) {
-                cmd.append(String.format(" %s%%", correlation));
+    private static void appendReordering(StringBuilder cmd, NetworkParameters params) {
+        if (params.isReorderingValueSet()) {
+            cmd.append(String.format(" reorder %s%%", params.getReorderingValue()));
+            if (params.isReorderingCorrelationSet()) {
+                cmd.append(String.format(" %s%%", params.getReorderingCorrelation()));
             }
         }
     }
 
-    private static void appendDuplication(StringBuilder cmd, int duplication, int correlation) {
-        if (duplication != -1) {
-            cmd.append(String.format(" duplicate %s%%", duplication));
-            if (correlation != -1) {
-                cmd.append(String.format(" %s%%", correlation));
+    private static void appendDuplication(StringBuilder cmd, NetworkParameters params) {
+        if (params.isDuplicationValueSet()) {
+            cmd.append(String.format(" duplicate %s%%", params.getDuplicationValue()));
+            if (params.isDuplicationCorrelationSet()) {
+                cmd.append(String.format(" %s%%", params.getDuplicationCorrelation()));
             }
         }
     }
 
-    private static void appendCorruption(StringBuilder cmd, int corruption) {
-        if (corruption != -1) {
-            cmd.append(String.format(" corrupt %s%%", corruption));
+    private static void appendCorruption(StringBuilder cmd, NetworkParameters params) {
+        if (params.isCorruptionSet()) {
+            cmd.append(String.format(" corrupt %s%%", params.getCorruption()));
         }
     }
 
@@ -84,38 +86,39 @@ public class TrafficControl {
         StringBuilder cmd = new StringBuilder();
         cmd.append(String.format("tc filter add dev %s protocol ip parent 1:0 prio 1 u32", dev));
 
-        if (params.getSrcAddress() != null && !params.getSrcAddress().isEmpty()) {
+        if (params.isSrcAddressSet() && params.isSrcSubnetMaskSet()) {
             cmd.append(String.format(" match ip src %s%s", params.getSrcAddress(), params.getSrcSubnetMask()));
         }
 
-        if (params.getDstAddress() != null && !params.getDstAddress().isEmpty()) {
-            cmd.append(String.format(" match ip dst %s", params.getDstAddress(), params.getDstSubnetMask()));
+        if (params.isDstAddressSet() && params.isDstSubnetMaskSet()) {
+            cmd.append(String.format(" match ip dst %s%s", params.getDstAddress(), params.getDstSubnetMask()));
         }
 
-        switch (params.getIpProtocol()) {
-            case NetworkEmulatorConstants.UDP_PROTOCOL:
-                cmd.append(" match ip protocol 17 0xff");
-                break;
-            case NetworkEmulatorConstants.ICMP_PROTOCOL:
-                cmd.append(" match ip protocol 1 0xff");
-                break;
-            case NetworkEmulatorConstants.TCP_PROTOCOL:
-            default:
-                cmd.append(" match ip protocol 6 0xff");
-                break;
+        if (params.isIpProtocolSet()) {
+            switch (params.getIpProtocol()) {
+                case NetworkEmulatorConstants.UDP_PROTOCOL:
+                    cmd.append(" match ip protocol 17 0xff");
+                    break;
+                case NetworkEmulatorConstants.ICMP_PROTOCOL:
+                    cmd.append(" match ip protocol 1 0xff");
+                    break;
+                case NetworkEmulatorConstants.TCP_PROTOCOL:
+                default:
+                    cmd.append(" match ip protocol 6 0xff");
+                    break;
+            }
         }
 
-        if (params.getSrcPort() != -1) {
+        if (params.isSrcPortSet()) {
             cmd.append(String.format(" match ip sport %d 0xffff", params.getSrcPort()));
         }
 
-        if (params.getDstPort() != -1) {
+        if (params.isDstPortSet()) {
             cmd.append(String.format(" match ip dport %d 0xffff", params.getDstPort()));
         }
 
         cmd.append(String.format(" flowid %s", classId));
 
-        System.out.println("filter command " + cmd.toString());
         CommandRunner.runCommand(cmd.toString());
     }
 
