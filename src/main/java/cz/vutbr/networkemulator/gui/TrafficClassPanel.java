0ebec11ea0e01e6dc047d383ba644e92cfbe6030
xbilek26 <bilekf@email.cz>
2025-04-24 09:17:50 +0200

    Refactor TrafficClassPanel: replace network parameters handling with Filter and Parameter classes

--- src/main/java/cz/vutbr/networkemulator/gui/TrafficClassPanel.java
+++ src/main/java/cz/vutbr/networkemulator/gui/TrafficClassPanel.java
@@ -16,9 +16,20 @@ import javax.swing.JPanel;
 import javax.swing.JRadioButton;
 import javax.swing.JTextField;
 
+import org.apache.jmeter.testelement.TestElement;
+import org.apache.jmeter.testelement.property.CollectionProperty;
+import org.apache.jmeter.testelement.property.JMeterProperty;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import cz.vutbr.networkemulator.model.filter.Filter;
+import cz.vutbr.networkemulator.model.parameters.Corruption;
+import cz.vutbr.networkemulator.model.parameters.Delay;
+import cz.vutbr.networkemulator.model.parameters.Duplication;
+import cz.vutbr.networkemulator.model.parameters.Loss;
+import cz.vutbr.networkemulator.model.parameters.Parameter;
+import cz.vutbr.networkemulator.model.parameters.Rate;
+import cz.vutbr.networkemulator.model.parameters.Reordering;
 import cz.vutbr.networkemulator.utils.NetworkEmulatorConstants;
 import cz.vutbr.networkemulator.verification.IpAddressVerifier;
 import cz.vutbr.networkemulator.verification.RangeVerifier;
@@ -27,6 +38,9 @@ public class TrafficClassPanel extends JPanel {
 
     @SuppressWarnings("unused")
     private static final Logger log = LoggerFactory.getLogger(TrafficClassPanel.class);
+
+    private final String PROPERTY_FILTER = "filter_";
+    private final String PROPERTY_PARAMETERS = "parameters_";
 
     private ButtonGroup ipProtocolGroup;
     private JRadioButton tcpButton;
@@ -59,55 +73,47 @@ public class TrafficClassPanel extends JPanel {
         init();
     }
 
-    public List<String> getNetworkParameters() {
-        List<String> parameters = new ArrayList<>();
-        parameters.add(getIpProtocol());
-        parameters.add(srcAddressField.getText());
-        parameters.add((String) srcSubnetMaskBox.getSelectedItem());
-        parameters.add(srcPortField.getText());
-        parameters.add(dstAddressField.getText());
-        parameters.add((String) dstSubnetMaskBox.getSelectedItem());
-        parameters.add(dstPortField.getText());
-        parameters.add(delayValueField.getText());
-        parameters.add(jitterField.getText());
-        parameters.add(delayCorrelationField.getText());
-        parameters.add((String) distributionsBox.getSelectedItem());
-        parameters.add(lossValueField.getText());
-        parameters.add(lossCorrelationField.getText());
-        parameters.add(rateField.getText());
-        parameters.add(reorderingValueField.getText());
-        parameters.add(reorderingCorrelationField.getText());
-        parameters.add(duplicationValueField.getText());
-        parameters.add(duplicationCorrelationField.getText());
-        parameters.add(corruptionField.getText());
+    public Filter getFilter() {
+        Filter filter = new Filter();
+        filter.setIpProtocol(getIpProtocol());
+        filter.setSrcAddress(srcAddressField.getText());
+        filter.setSrcSubnetMask((String) srcSubnetMaskBox.getSelectedItem());
+        filter.setSrcPort(srcPortField.getText());
+        filter.setDstAddress(dstAddressField.getText());
+        filter.setDstSubnetMask((String) dstSubnetMaskBox.getSelectedItem());
+        filter.setDstPort(dstPortField.getText());
 
-        parameters.add((String) protocolsBox.getSelectedItem());
-
-        return parameters;
+        return filter;
     }
 
-    public void setNetworkParameters(List<String> parameters) {
-        setIpProtocol(parameters.get(0));
-        srcAddressField.setText(parameters.get(1));
-        srcSubnetMaskBox.setSelectedItem(parameters.get(2));
-        srcPortField.setText(parameters.get(3));
-        dstAddressField.setText(parameters.get(4));
-        dstSubnetMaskBox.setSelectedItem(parameters.get(5));
-        dstPortField.setText(parameters.get(6));
-        delayValueField.setText(parameters.get(7));
-        jitterField.setText(parameters.get(8));
-        delayCorrelationField.setText(parameters.get(9));
-        distributionsBox.setSelectedItem(parameters.get(10));
-        lossValueField.setText(parameters.get(11));
-        lossCorrelationField.setText(parameters.get(12));
-        rateField.setText(parameters.get(13));
-        reorderingValueField.setText(parameters.get(14));
-        reorderingCorrelationField.setText(parameters.get(15));
-        duplicationValueField.setText(parameters.get(16));
-        duplicationCorrelationField.setText(parameters.get(17));
-        corruptionField.setText(parameters.get(18));
+    public List<Parameter> getParameters() {
+        List<Parameter> parameters = new ArrayList<>();
+        parameters.add(new Delay(
+                delayValueField.getText(),
+                jitterField.getText(),
+                delayCorrelationField.getText(),
+                (String) distributionsBox.getSelectedItem()
+        ));
+        parameters.add(new Loss(
+                lossValueField.getText(),
+                lossCorrelationField.getText()
+        ));
+        parameters.add(new Rate(
+                rateField.getText()
+        ));
+        parameters.add(new Reordering(
+                reorderingValueField.getText(),
+                reorderingCorrelationField.getText()
+        ));
+        parameters.add(new Duplication(
+                duplicationValueField.getText(),
+                duplicationCorrelationField.getText()
+        ));
+        parameters.add(new Corruption(
+                corruptionField.getText()
+        ));
 
-        protocolsBox.setSelectedItem(parameters.get(19));
+        return parameters;
     }
 
     private void init() {
@@ -322,7 +328,6 @@ public class TrafficClassPanel extends JPanel {
     }
 
     private String getIpProtocol() {
-        System.out.println("ip protocol " + ipProtocolGroup.getSelection().getActionCommand());
         if (udpButton.isSelected()) {
             return NetworkEmulatorConstants.UDP_PROTOCOL;
         }
@@ -360,4 +365,59 @@ public class TrafficClassPanel extends JPanel {
         }
     }
 
+    public void modifyTestElement(TestElement te) {
+        CollectionProperty filter = new CollectionProperty(PROPERTY_FILTER + getName(), new ArrayList<>());
+        filter.addItem(getIpProtocol());
+        filter.addItem(srcAddressField.getText());
+        filter.addItem((String) srcSubnetMaskBox.getSelectedItem());
+        filter.addItem(srcPortField.getText());
+        filter.addItem(dstAddressField.getText());
+        filter.addItem((String) dstSubnetMaskBox.getSelectedItem());
+        filter.addItem(dstPortField.getText());
+        te.setProperty(filter);
+
+        CollectionProperty parameters = new CollectionProperty(PROPERTY_PARAMETERS + getName(), new ArrayList<>());
+        parameters.addItem(delayValueField.getText());
+        parameters.addItem(jitterField.getText());
+        parameters.addItem(delayCorrelationField.getText());
+        parameters.addItem((String) distributionsBox.getSelectedItem());
+        parameters.addItem(lossValueField.getText());
+        parameters.addItem(lossCorrelationField.getText());
+        parameters.addItem(rateField.getText());
+        parameters.addItem(reorderingValueField.getText());
+        parameters.addItem(reorderingCorrelationField.getText());
+        parameters.addItem(duplicationValueField.getText());
+        parameters.addItem(duplicationCorrelationField.getText());
+        parameters.addItem(corruptionField.getText());
+        te.setProperty(parameters);
+    }
+
+    void configure(TestElement te) {
+        JMeterProperty filterPropery = (CollectionProperty) te.getProperty(PROPERTY_FILTER + getName());
+        if (filterPropery instanceof CollectionProperty filter) {
+            setIpProtocol(filter.get(0).getStringValue());
+            srcAddressField.setText(filter.get(1).getStringValue());
+            srcSubnetMaskBox.setSelectedItem(filter.get(2).getStringValue());
+            srcPortField.setText(filter.get(3).getStringValue());
+            dstAddressField.setText(filter.get(4).getStringValue());
+            dstSubnetMaskBox.setSelectedItem(filter.get(5).getStringValue());
+            dstPortField.setText(filter.get(6).getStringValue());
+        }
+
+        JMeterProperty parametersProperty = te.getPropertyOrNull(PROPERTY_PARAMETERS + getName());
+        if (parametersProperty instanceof CollectionProperty parameters) {
+            delayValueField.setText(parameters.get(0).getStringValue());
+            jitterField.setText(parameters.get(1).getStringValue());
+            delayCorrelationField.setText(parameters.get(2).getStringValue());
+            distributionsBox.setSelectedItem(parameters.get(3).getStringValue());
+            lossValueField.setText(parameters.get(4).getStringValue());
+            lossCorrelationField.setText(parameters.get(5).getStringValue());
+            rateField.setText(parameters.get(6).getStringValue());
+            reorderingValueField.setText(parameters.get(7).getStringValue());
+            reorderingCorrelationField.setText(parameters.get(8).getStringValue());
+            duplicationValueField.setText(parameters.get(9).getStringValue());
+            duplicationCorrelationField.setText(parameters.get(10).getStringValue());
+            corruptionField.setText(parameters.get(11).getStringValue());
+        }
+    }
 }
